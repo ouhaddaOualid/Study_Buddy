ed8c01a4f8ea088b125688adedf7a77e6be6a94b
ouhaddaOualid <83510523+ouhaddaOualid@users.noreply.github.com>
2022-04-11 11:18:52 +0000

    change

--- src/main/java/com/ProjetIntegre/BackEnd/Enteties/FormationProgramme.java
+++ src/main/java/com/ProjetIntegre/BackEnd/Enteties/FormationProgramme.java
@@ -1,39 +1,15 @@
 package com.ProjetIntegre.BackEnd.Enteties;
 
 import java.util.Date;
-
 public class FormationProgramme {
     private Formation formation;
-    private Instructeur instructeur ;
-    private Date[] datDispo ;
+    private Instructeur instructeur;
+    private Date datDispo;
 
-    public FormationProgramme(Formation formation, Instructeur instructeur, Date[] datDispo) {
+    public FormationProgramme(com.ProjetIntegre.BackEnd.Enteties.Formation formation, com.ProjetIntegre.BackEnd.Enteties.Instructeur instructeur, Date datDispo) {
         this.formation = formation;
         this.instructeur = instructeur;
-        this.datDispo = datDispo;
-    }
-
-    public Formation getFormation() {
-        return formation;
-    }
-
-    public void setFormation(Formation formation) {
-        this.formation = formation;
-    }
-
-    public Instructeur getInstructeur() {
-        return instructeur;
-    }
-
-    public void setInstructeur(Instructeur instructeur) {
-        this.instructeur = instructeur;
-    }
-
-    public Date[] getDatDispo() {
-        return datDispo;
-    }
-
-    public void setDatDispo(Date[] datDispo) {
         this.datDispo = datDispo;
     }
 }
+
